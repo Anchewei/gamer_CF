21d892314169fe90305f8d98c1d24eaba1e8e104
Anchewei <10302leo@gamil.com>
2023-02-12 11:50:06 +0800

    debug

--- src/StarFormation/SF_CreateStar_AGORA.cpp
+++ src/StarFormation/SF_CreateStar_AGORA.cpp
@@ -514,22 +514,8 @@ void SF_CreateStar_AGORA( const int lv, const real TimeNew, const real dt, Rando
 
             Egtot += 0.5*vifluid[DENS]*dv*phiijk;
          } // vii, vji, vki
-#        ifdef MY_DEBUG
-         real BoxCenter[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
-         real dx, dy, dz;
-         dx = x - BoxCenter[0];
-         dy = y - BoxCenter[1];
-         dz = z - BoxCenter[2];
-         real D2O = SQRT(SQR(dx)+SQR(dy)+SQR(dz));
-         if (D2O < 0.5*AccRadius)
-         {
-         fprintf( File, "%d %d",  PID, NotMiniPot);
-         fprintf( File, "\n" );
-         }
-         // fprintf( File, "'%13.7e %13.7e %13.7e',",  x, y, z);
-         // fprintf( File, "\n" );
-#        endif
-         if ( NotMiniPot )                                   continue;
+
+         // if ( NotMiniPot )                                   continue;
 
          real Ethtot = (real)0.0, Emagtot = (real)0.0, Ekintot = (real)0.0;
          for (int vk=pk-AccCellNum; vk<=pk+AccCellNum; vk++)
@@ -565,7 +551,21 @@ void SF_CreateStar_AGORA( const int lv, const real TimeNew, const real dt, Rando
 
             Ekintot += 0.5*vfluid[DENS]*dv*( SQR(vfluid[MOMX]/vfluid[DENS] - MWvel[0]) + SQR(vfluid[MOMY]/vfluid[DENS] - MWvel[1]) + SQR(vfluid[MOMZ]/vfluid[DENS] - MWvel[2]));
          } // vi, vj, vk
-
+#        ifdef MY_DEBUG
+         real BoxCenter[3] = { amr->BoxCenter[0], amr->BoxCenter[1], amr->BoxCenter[2] };
+         real dx, dy, dz;
+         dx = x - BoxCenter[0];
+         dy = y - BoxCenter[1];
+         dz = z - BoxCenter[2];
+         real D2O = SQRT(SQR(dx)+SQR(dy)+SQR(dz));
+         if (D2O < 0.5*AccRadius)
+         {
+         fprintf( File, "%d %d %d %d",  PID, NotMiniPot, FABS(Egtot) <= 2*Ethtot, ( Egtot + Ethtot + Ekintot + Emagtot ) >= 0);
+         fprintf( File, "\n" );
+         }
+         // fprintf( File, "'%13.7e %13.7e %13.7e',",  x, y, z);
+         // fprintf( File, "\n" );
+#        endif
          if ( FABS(Egtot) <= 2*Ethtot)                       continue;
          if (( Egtot + Ethtot + Ekintot + Emagtot ) >= 0)    continue;
 
