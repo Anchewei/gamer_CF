45193f5cc7ea4f1f62010d985c8ae4db61108e2c
Anchewei <10302leo@gamil.com>
2023-04-17 11:30:13 +0800

    try to fix FEEDBACK

--- src/Auxiliary/Aux_Check_Parameter.cpp
+++ src/Auxiliary/Aux_Check_Parameter.cpp
@@ -461,9 +461,9 @@ void Aux_Check_Parameter()
 #  endif
 
 // for sending fluid data fixed by coarse-fine fluxes correctly
-   if ( OPT__FIXUP_FLUX  &&  Flu_ParaBuf >= PATCH_SIZE )
-      Aux_Error( ERROR_INFO, "\"%s\" is required for \"%s\" in LOAD_BALANCE --> check LB_RecordExchangeFixUpDataPatchID() !!\n",
-                 "Flu_ParaBuf < PATCH_SIZE", "OPT__FIXUP_FLUX" );
+   // if ( OPT__FIXUP_FLUX  &&  Flu_ParaBuf >= PATCH_SIZE )
+   //    Aux_Error( ERROR_INFO, "\"%s\" is required for \"%s\" in LOAD_BALANCE --> check LB_RecordExchangeFixUpDataPatchID() !!\n",
+   //               "Flu_ParaBuf < PATCH_SIZE", "OPT__FIXUP_FLUX" );
 
 // ensure that the variable "PaddedCr1D" will not overflow
    const int Padded              = 1<<NLEVEL;
